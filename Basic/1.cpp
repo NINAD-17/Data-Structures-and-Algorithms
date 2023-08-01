3c7596534f986b046ed33e84c3d7f13fd3abc5f2
Ninad <ninadkdhulap17@outlook.com>
2023-08-01 16:56:50 +0530

    Create Readme.md and type casting in cpp file
    Learn how type cast work in cpp & integer value range

---
+++ Basic/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Type Cast in CPP
+    int a = 'a';
+    cout << "Value of a using type casting: " << a << endl;
+
+    char b = 98;
+    cout << "Value of b using type casting: " << b << endl;
+
+    char c = 123456;
+    cout << "Value of c: " << c << endl; // Read Detailed explaination in readme.md file -> Type Casting
+
+    int modulo = 123456 % 256;
+    char d = modulo;
+    cout << "12345 % 256 = " << modulo << endl;
+    cout << "Character with value " << modulo << " in ASCII table: " << d << endl;
+}
+

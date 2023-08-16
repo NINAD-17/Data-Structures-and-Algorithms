feb21e511335ef5b0596472fb6d68229144fa64f
Ninad <theninad17@gmail.com>
2023-08-16 20:45:24 +0530

    add brute force solution of book allocation problem

--- Arrays/Book_Allocation.cpp
+++ Arrays/Book_Allocation.cpp
@@ -4,8 +4,49 @@ using namespace std;
 
 // Problem Link: https://www.codingninjas.com/studio/library/book-allocation-problem
 
+bool isPossible(int *bookPages, int numOfBooks, int numOfStud, int numOfPages) {
+    int cntStud = 1;
+    int currSum = 0;
+
+    for(int i = 0; i < numOfBooks; i++) {
+        if(bookPages[i] > numOfPages)
+            return false;
+        
+        if(currSum + bookPages[i] > numOfPages) {
+            cntStud++;
+            currSum += bookPages[i];
+
+            if(cntStud > numOfStud)
+                return false;
+        } else {
+            currSum += bookPages[i];
+        }
+    }
+    return true;
+}
+
+// Brute Force
+int bookAllocation(int *books, int numOfBooks, int numOfStud) {
+    // If num of students are greater than total num of books
+    if(numOfStud > numOfBooks)
+        return -1;
+
+    int totalPages = 0;
+    // Count Total pages
+    for(int i = 0; i < numOfBooks; i++)
+        totalPages += books[i];
+    
+    // Check every possible value
+    for(int numPages = 1; numPages <= totalPages; numPages++) {
+        if(isPossible(books, numOfBooks, numOfStud, numPages)) 
+            return numPages;
+    }
+    return -1;
+}
+
 // Brute Force Solution
 // Time Complexity: O(n^2)
+// In this logic we're assuming that there are only two students
 int bookAllocation_bruteForce(int *books, int noOfBooks) {
     int ans = INT_MAX, sum1 = 0;
     
@@ -31,15 +72,19 @@ int bookAllocation_bruteForce(int *books, int noOfBooks) {
 }
 
 int main() {
-    int books[10], noOfBooks;
+    int books[10], numOfBooks, studNum;
 
     cout << "Enter total number of books: ";
-    cin >> noOfBooks;
+    cin >> numOfBooks;
 
     cout << "Enter total pages of each book: ";
-    for(int i = 0; i < noOfBooks; i++)
+    for(int i = 0; i < numOfBooks; i++)
         cin >> books[i];
 
-    cout << "The minimum of the maximum number of pages assigned is " << bookAllocation_bruteForce(books, noOfBooks) << endl;
+    cout << "Enter total number of students: ";
+    cin >> studNum;
+
+    cout << "The minimum of the maximum number of pages assigned to only 2 students is " << bookAllocation_bruteForce(books, numOfBooks) << endl;
+    cout << "The minimum of the maximum number of pages assigned to " << studNum << " students is " << bookAllocation(books, numOfBooks, studNum) << endl;
     return 0;
 }

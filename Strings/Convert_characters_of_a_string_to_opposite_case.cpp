57440dfc8a1413c6cc48c62226b831692660d183
Ninad <theninad17@gmail.com>
2023-09-03 20:07:17 +0530

    solve convert string char to opposite case

---
+++ Strings/Convert_characters_of_a_string_to_opposite_case.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void convertToOppCase(string &str) {
+    for(int i = 0; i < str.length(); i++) {
+        if(isupper(str[i])) 
+            str[i] = tolower(str[i]);
+        else
+            str[i] = toupper(str[i]);
+    }
+}
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    convertToOppCase(str);
+    cout << str << endl;
+    return 0;
+}

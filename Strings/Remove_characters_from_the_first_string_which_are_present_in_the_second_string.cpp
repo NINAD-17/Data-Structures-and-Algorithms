4d764a02fe42f252a90b03f43a76dd46775e1a1b
Ninad <theninad17@gmail.com>
2023-09-04 19:44:39 +0530

    update remove char solution by changing i/p string itself with answer

--- Strings/Remove_characters_from_the_first_string_which_are_present_in_the_second_string.cpp
+++ Strings/Remove_characters_from_the_first_string_which_are_present_in_the_second_string.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Link: https://www.geeksforgeeks.org/remove-characters-from-the-first-string-which-are-present-in-the-second-string/
+
 // time complexity: O(n)
+// resize function: The resize function is a member function of the std::string class. It changes the size of the string to the specified size k. If k is less than the current size of the string, the function removes characters from the end of the string. If k is greater than the current size of the string, the function appends additional characters to the end of the string. The time complexity of this operation is O(k) where k is the new size of the string.
 string removeChars(string str1, string str2) {
     int alphabates[26] = {0};
     string ans = "";
+    int k = 0;
 
     for(int i = 0; i < str2.length(); i++)
         alphabates[str2[i] - 'a']++;
@@ -13,8 +17,11 @@ string removeChars(string str1, string str2) {
         if(alphabates[str1[i] - 'a'] > 0)
             continue;
         ans += str1[i];
+        str1[k++] = str1[i];
     }
 
+    str1.resize(k);
+    cout << str1 << endl;
     return ans;
 }
 

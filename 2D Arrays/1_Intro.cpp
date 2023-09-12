270a5c90c1757c28e3637a10b72627b1b9d854ed
Ninad <theninad17@gmail.com>
2023-09-12 20:16:37 +0530

    add two ways to pass 2D arr in parameter

--- 2D Arrays/1_Intro.cpp
+++ 2D Arrays/1_Intro.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// Passing static size to 2D array in parameters is not recommended by C++.
+// Instead you can use
+// 1. Pass a 2D array to a function using pointers and sizes
+void print2D_ArrUsingPtrAndSizes(int *arr, int row, int col) {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            if(j == col - 1) 
+                cout << "| " << *((arr + (i * col)) + j) << " |";
+            else 
+                cout << "| " << *((arr + (i * col)) + j) << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// 2. Using Vector of vector
+void print2D_ArrUsingVector(vector<vector<int>> &arr) {
+    for(int row = 0; row < arr.size(); row++) {
+        for(int col = 0; col < arr[row].size(); col++) {
+            if(col == arr[row].size() - 1)
+                cout << "| " << arr[row][col] << " |";
+            else
+                cout << "| " << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
 bool isPresent(int arr[][4], int row, int col, int target) {
     // Read reason of why we must have to include column size in parameters on readme.md
@@ -16,6 +47,11 @@ int main() {
 
     int arr1[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // 3 * 4 = 12 elements
     int arr2[3][4] = {{5, 7, 8, 9}, {1, 2, 3, 4}, {6, 10, 12, 11}}; // To put specific values in specific columns and rows
+    print2D_ArrUsingPtrAndSizes((int *)arr1, 3, 4);
+    print2D_ArrUsingPtrAndSizes((int *)arr2, 3, 4);
+
+    // Using Vector
+    vector<vector<int>> arr(3, vector<int>(3)) ;
 
     int arr[10][10];
 

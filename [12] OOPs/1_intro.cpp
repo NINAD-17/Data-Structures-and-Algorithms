3ac13fbbc52898beac3798754e7b050c86deb71f
Ninad <ninadkdhulap17@outlook.com>
2024-06-01 18:12:19 +0530

    learn OOPs basics

---
+++ [12] OOPs/1_intro.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+// #include "External Classes/1_intro.cpp"
+using namespace std;
+
+// Class defined in same file
+class Hero {
+    // Properties
+    public:
+        // int health;
+        char level;
+};
+
+int main() {
+
+    // Creation of object
+    Hero ironman;
+
+    // Updating values of object members
+    // ironman.health = 100;
+    ironman.level = 'A';
+
+    cout << "Size of hero: " << sizeof(ironman) << endl;
+    // cout << "Health of ironman: " << ironman.health << endl;
+    cout << "Level of ironman: " << ironman.level << endl;
+
+
+    return 0;
+}

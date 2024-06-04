87872b309541961f7d3d5950ac581fda972591a2
Ninad <ninadkdhulap17@outlook.com>
2024-06-04 21:16:22 +0530

    learn doubly circular linked list

---
+++ [13] Linked List/4_Doubly_Circular_Linked_List.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node* next;
+        Node* prev;
+
+        // constructor
+        Node(int data) {
+            this -> data = data;
+            this -> next = NULL;
+            this -> prev = NULL;
+        }
+
+        // destructor
+};
+
+void createLL(Node* &tail, int data) {
+    // allocate memory in heap for new node
+    Node* newNode = new Node(data);
+    newNode -> next = newNode;
+    newNode -> prev = newNode;
+    tail = newNode;
+    cout << "new linkedlist created!" << endl;
+}
+
+void insertNode(Node* &tail, int element, int data) {
+    // empty linked list
+    if(tail == NULL) {
+        createLL(tail, data);
+        return;
+    }
+
+    // non-empty linked list
+    Node* tempNode = tail -> next;
+    while(tempNode -> data != element) {
+        if(tempNode == tail) {
+            break;
+        }
+        tempNode = tempNode -> next;
+    }
+
+    if(tempNode -> data != element && tempNode == tail) {
+        cout << "element not found in linked list :(" << endl;
+        return;
+    }
+
+    Node* newNode = new Node(data);
+    if(tempNode -> data == element && tempNode == tail) 
+        tail = newNode;
+
+    newNode -> next = tempNode -> next;
+    tempNode -> next -> prev = newNode;
+    tempNode -> next = newNode;
+    newNode -> prev = tempNode;
+}
+
+void deleteNode(Node* &tail, int nodeToDelete) {
+    // if linked list isn't exist
+    if(tail == NULL) {
+        cout << "linked list isn't exist!" << endl;
+        return;
+    }
+
+    // only one node exist
+    if(tail -> next == tail && tail -> data == nodeToDelete) {
+        tail -> next = NULL;
+        tail -> prev = NULL;
+        delete tail;
+        tail = NULL;
+        return;
+    }
+
+    // more than one node exist
+    Node* prevNode = tail;
+    Node* currNode = tail -> next;
+
+    while(currNode -> data != nodeToDelete) {
+        if(currNode == tail)
+            break;
+        prevNode = currNode;
+        currNode = currNode -> next;
+    }
+
+    if(currNode -> data != nodeToDelete && currNode == tail) {
+        cout << "--- please enter a valid node to delete ---" << endl;
+        return;
+    }
+
+    if(currNode == tail) 
+        tail = currNode -> next;
+    
+    prevNode -> next = currNode -> next;
+    currNode -> next -> prev = prevNode;
+    currNode -> next = NULL;
+    currNode -> prev = NULL;
+    delete currNode;
+}
+
+void printLL(Node* &tail) {
+    Node* temp = tail;
+
+    if(tail == NULL) {
+        cout << "linked list is empty or not exist :(" << endl;
+        cout << "tail node: " << tail << endl << endl;
+        return;
+    }
+
+    cout << "\nprinting LL: ";
+    do {
+        cout << temp -> data << " -> ";
+        temp = temp -> next;
+    } while (temp != tail);
+
+    cout << "NULL" << endl;
+    cout << "tail: " << tail->data << endl;
+}
+
+int main() {
+
+    Node* tail = NULL;
+    insertNode(tail, 4, 10);
+    insertNode(tail, 10, 20);
+    insertNode(tail, 20, 30);
+    insertNode(tail, 10, 40);
+    printLL(tail);
+
+    deleteNode(tail, 40);
+    printLL(tail);
+
+    deleteNode(tail, 20);
+    printLL(tail);
+
+    deleteNode(tail, 30);
+    printLL(tail);
+
+    deleteNode(tail, 22);
+    printLL(tail);
+    
+    deleteNode(tail, 10);
+    printLL(tail);
+
+    return 0;
+}

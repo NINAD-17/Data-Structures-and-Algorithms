5e4968a29c13dfbb26384ff6dfd9ed0a712c6f3f
Ninad <ninadkdhulap17@outlook.com>
2024-06-19 00:28:40 +0530

    fix small error in an insertion sort problem

--- [02] Arrays/Sorting Algorithms/Insertion_Sort.cpp
+++ [02] Arrays/Sorting Algorithms/Insertion_Sort.cpp
@@ -8,7 +8,7 @@ int insertionSort(int *arr, int size) {
         int temp = arr[i];
         int j = i - 1;
         for(; j >= 0; j--) {
-            if(arr[j] > arr[i])
+            if(arr[j] > temp)
                 arr[j + 1] = arr[j];
             else
                 break;
